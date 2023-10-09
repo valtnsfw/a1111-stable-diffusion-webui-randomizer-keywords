dea85584114c225de9a945b3bedeaef06cae977a
Nikita K <krochak_n@mail.ru>
2023-10-09 19:56:54 +1000

    Remove stuff anyone will most likely never use

--- scripts/keywords.py
+++ scripts/keywords.py
@@ -1,6 +1,6 @@
 import re
 import sys
-from modules import scripts, script_callbacks, ui_extra_networks, extra_networks, shared, sd_models, sd_vae, sd_samplers, processing, rng
+from modules import scripts, script_callbacks, extra_networks, shared, sd_models, sd_samplers, processing, rng
 
 
 operations = {
@@ -48,60 +48,28 @@ def recalc_hires_fix(p):
             print_params(p)
 
 
-class RandomizerKeywordConfigOption(extra_networks.ExtraNetwork):
-    def __init__(self, keyword_name, param_type, value_min=0, value_max=None, option_name=None, validate_cb=None, adjust_cb=None):
-        super().__init__(keyword_name)
-        self.param_type = param_type
-        self.value_min = value_min
-        self.value_max = value_max
-        self.validate_cb = validate_cb
-        self.adjust_cb = adjust_cb
+def get_xyz_plot_axe_param_names() -> set[int]:
+    xyz_plot_axe_param_names = set()
+    for axe in {'x', 'y', 'z'}:
+        xyz_plot_axe_param_names.add(f"xyz_plot_{axe}")
+    return xyz_plot_axe_param_names
 
-        self.option_name = option_name
-        if self.option_name is None:
-            self.option_name = keyword_name
 
-        self.has_original = False
-        self.original_value = None
+def is_xyz_plot() -> bool:
+    for xyz_plot_axe_param_name in get_xyz_plot_axe_param_names():
+        xyz_plot_axe = getattr(shared.state, xyz_plot_axe_param_name, None)
+        if xyz_plot_axe is not None and len(xyz_plot_axe.values) > 0:
+            return True
+    return False
 
-    def activate(self, p, params_list):
-        if not params_list:
-            return
 
-        if not self.has_original:
-            self.original_value = shared.opts.data[self.option_name]
-            self.has_original = True
-
-        value = params_list[0].items[0]
-        value = self.param_type(value)
-
-        if self.adjust_cb:
-            value = self.adjust_cb(value, p)
-
-        if isinstance(value, int) or isinstance(value, float):
-            if self.value_min:
-                value = max(value, self.value_min)
-            if self.value_max:
-                value = min(value, self.value_max)
-
-        if self.validate_cb:
-            error = self.validate_cb(value, p)
-            if error:
-                raise RuntimeError(f"Validation for '{self.name}' keyword failed: {error}")
-
-        if is_debug():
-            print(f"[RandomizerKeywords] Set CONFIG option: {self.option_name} -> {value}")
-
-        shared.opts.data[self.option_name] = value
-
-    def deactivate(self, p):
-        if self.has_original:
-            if is_debug():
-                print(f"[RandomizerKeywords] Reset CONFIG option: {self.option_name} -> {self.original_value}")
-
-            shared.opts.data[self.option_name] = self.original_value
-            self.has_original = False
-            self.original_value = None
+def xyz_plot_grid_size() -> int:
+    size = 1
+    for xyz_plot_axe_param_name in get_xyz_plot_axe_param_names():
+        xyz_plot_axe = getattr(shared.state, xyz_plot_axe_param_name, None)
+        if xyz_plot_axe is not None:
+            size *= max(1, len(xyz_plot_axe.values))
+    return size
 
 
 class RandomizerKeywordSamplerParam(extra_networks.ExtraNetwork):
@@ -170,29 +138,6 @@ def validate_sampler_name(x, p):
         return f"Invalid sampler '{x}'"
     return None
 
-def get_xyz_plot_axe_param_names() -> set[int]:
-    xyz_plot_axe_param_names = set()
-    for axe in {'x', 'y', 'z'}:
-        xyz_plot_axe_param_names.add(f"xyz_plot_{axe}")
-
-    return xyz_plot_axe_param_names
-
-def is_xyz_plot() -> bool:
-    for xyz_plot_axe_param_name in get_xyz_plot_axe_param_names():
-        xyz_plot_axe = getattr(shared.state, xyz_plot_axe_param_name, None)
-        if xyz_plot_axe is not None and len(xyz_plot_axe.values) > 0:
-            return True
-
-    return False
-
-def xyz_plot_grid_size() -> int:
-    size = 1
-    for xyz_plot_axe_param_name in get_xyz_plot_axe_param_names():
-        xyz_plot_axe = getattr(shared.state, xyz_plot_axe_param_name, None)
-        if xyz_plot_axe is not None:
-            size *= max(1, len(xyz_plot_axe.values))
-
-    return size
 
 class RandomizerKeywordCheckpoint(extra_networks.ExtraNetwork):
     def __init__(self):
@@ -231,131 +176,6 @@ class RandomizerKeywordCheckpoint(extra_networks.ExtraNetwork):
             self.original_checkpoint_info = None
 
 
-class RandomizerKeywordVAE(extra_networks.ExtraNetwork):
-    def __init__(self):
-        super().__init__("vae")
-        self.has_original = False
-        self.original_vae_info = None
-
-    def find_vae(self, name: str):
-        if name.lower() in ['auto', 'automatic']:
-            return sd_vae.unspecified
-        if name.lower() == 'none':
-            return None
-        else:
-            choices = [x for x in sorted(sd_vae.vae_dict, key=lambda x: len(x)) if name.lower().strip() in x.lower()]
-            if len(choices) == 0:
-                return None
-            else:
-                return sd_vae.vae_dict[choices[0]]
-
-    def activate(self, p, params_list):
-        if not params_list:
-            return
-
-        if not self.has_original:
-            self.original_vae_info = shared.opts.sd_vae
-            self.has_original = True
-
-        params = params_list[0]
-        assert len(params.items) > 0, "Must provide VAE name or 'auto' for automatic"
-
-        name = params.items[0]
-        info = self.find_vae(name)
-        if info is None:
-            raise RuntimeError(f"Unknown VAE: {name}")
-
-        if is_debug():
-            print(f"[RandomizerKeywords] Set VAE: {info.name}")
-
-        sd_vae.reload_vae_weights(shared.sd_model, vae_file=info)
-
-    def deactivate(self, p):
-        if self.has_original:
-            # Disable for speedup
-            # if is_debug():
-                # print(f"[RandomizerKeywords] Reset VAE: {self.original_vae_info.name}")
-
-            # shared.opts.data["sd_vae"] = self.original_vae_info
-            # sd_vae.reload_vae_weights()
-
-            self.original_checkpoint_info = None
-            self.has_original = False
-
-
-def update_extension_args(ext_name, p, value, arg_idx):
-    if isinstance(p, processing.StableDiffusionProcessingImg2Img):
-        all_scripts = scripts.scripts_img2img.alwayson_scripts
-    else:
-        all_scripts = scripts.scripts_txt2img.alwayson_scripts
-
-    script_class = extension_classes[ext_name]
-    script = next(iter([s for s in all_scripts if isinstance(s, script_class)]), None)
-    assert script, f"Could not find script for {script_class}!"
-
-    args = list(p.script_args)
-
-    if is_debug():
-        print(f"[RandomizerKeywords] Args in {ext_name}: {args[script.args_from:script.args_to]}")
-        print(f"[RandomizerKeywords] For {ext_name}: Changed arg {arg_idx} from {args[script.args_from + arg_idx]} to {value}")
-
-    args[script.args_from + arg_idx] = value
-    p.script_args = tuple(args)
-
-
-class RandomizerKeywordExtAddNetModel(extra_networks.ExtraNetwork):
-    def __init__(self, index):
-        super().__init__(f"addnet_model_{index+1}")
-        self.index = i
-
-    def activate(self, p, params_list):
-        if not params_list:
-            return
-
-        model_util = sys.modules.get("scripts.model_util")
-        if not model_util:
-            raise RuntimeError("Could not load additional_networks model_util")
-
-        value = params_list[0].items[0]
-        name = model_util.find_closest_lora_model_name(value)
-        if not name:
-            raise RuntimeError(f"Could not find LoRA with name {value}")
-
-        update_extension_args("additional_networks", p, True, 0)
-        update_extension_args("additional_networks", p, name, 3 + 4 * self.index)  # enabled, separate_weights, (module, {model}, weight_unet, weight_tenc), ...
-
-    def deactivate(self, p):
-        pass
-
-
-class RandomizerKeywordExtAddNetWeight(extra_networks.ExtraNetwork):
-    def __init__(self, index, kind=None):
-        if kind is None:
-            name = f"addnet_weight_{index+1}"
-        else:
-            name = f"addnet_{kind}_weight_{index+1}"
-
-        super().__init__(name)
-        self.index = i
-        self.kind = kind
-
-    def activate(self, p, params_list):
-        if not params_list:
-            return
-
-        value = float(params_list[0].items[0])
-
-        # enabled, separate_weights, (module, model, {weight_unet, weight_tenc}), ...
-        update_extension_args("additional_networks", p, True, 0)
-        if self.kind is None or self.kind == "unet":
-            update_extension_args("additional_networks", p, value, 4 + 4 * self.index)
-        if self.kind is None or self.kind == "tenc":
-            update_extension_args("additional_networks", p, value, 5 + 4 * self.index)
-
-    def deactivate(self, p):
-        pass
-
-
 class Script(scripts.Script):
     def title(self):
         return "Randomizer Keywords"
@@ -390,13 +210,7 @@ class Script(scripts.Script):
             first_batch_entity_params = {}
 
 
-config_params = [
-    RandomizerKeywordConfigOption("clip_skip", int, 1, 12, option_name="CLIP_stop_at_last_layers")
-]
-
-
-# Sampler parameters that can be controlled. They are parameters in the
-# Processing class.
+# Sampler parameters that can be controlled. They are parameters in the Processing class.
 sampler_params = [
     RandomizerKeywordSamplerParam("cfg_scale", float, 1),
     RandomizerKeywordSamplerParam("seed", int, -1),
@@ -431,42 +245,10 @@ sampler_params = [
 
 other_params = [
     RandomizerKeywordCheckpoint(),
-    RandomizerKeywordVAE()
 ]
 
-
-extension_params = []
-extension_modules = {}
-extension_classes = {}
-supported_modules = {
-    "additional_networks": []
-}
-
-for i in range(5):
-    supported_modules["additional_networks"].extend([
-        RandomizerKeywordExtAddNetModel(i),
-        RandomizerKeywordExtAddNetWeight(i),
-        RandomizerKeywordExtAddNetWeight(i, "unet"),
-        RandomizerKeywordExtAddNetWeight(i, "tenc"),
-    ])
-
-
-all_params = []
-
-
 def on_app_started(demo, app):
-    global all_params
-
-    for s in scripts.scripts_data:
-        for m, params in supported_modules.items():
-            if s.module.__name__ == m + ".py":
-                assert m not in extension_modules
-                print(f"[RandomizerKeywords] Adding support for extension: {m}")
-                extension_modules[m] = s.module
-                extension_classes[m] = s.script_class
-                extension_params.extend(params)
-
-    all_params = config_params + sampler_params + other_params + extension_params
+    all_params = sampler_params + other_params
     print(f"[RandomizerKeywords] Supported keywords: {', '.join([p.name for p in all_params])}")
 
     for param in all_params:
